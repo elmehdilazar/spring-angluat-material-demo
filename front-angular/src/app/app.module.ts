2f8d034e6abcc31f3a5e5b9353ecf7cada527882
elmehdilazar <midomidomido01@gmail.Com>
2024-06-12 16:42:22 +0100

    app module

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -6,25 +6,41 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AdminTemplateComponent } from './admin-template/admin-template.component';
 import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
-import {MatButton, MatIconButton} from "@angular/material/button";
-import {MatIcon} from "@angular/material/icon";
-import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {MatButton, MatButtonModule, MatIconButton} from "@angular/material/button";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatMenu, MatMenuItem, MatMenuModule, MatMenuTrigger} from "@angular/material/menu";
+import {MatDrawer, MatSidenavModule} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { StudentComponent } from './student/student.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PaymentsComponent } from './payments/payments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminTemplateComponent
+    AdminTemplateComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoginComponent,
+    StudentComponent,
+    DashboardComponent,
+    PaymentsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatButton,
-    MatIconButton,
-    MatIcon,
+    MatButtonModule,
+    MatIconModule,
     MatMenuTrigger,
-    MatMenu,
-    MatMenuItem
+    MatMenuModule,
+    MatSidenavModule,
+    MatNavList,
+    MatListItem,
+
   ],
   providers: [
     provideClientHydration(),
